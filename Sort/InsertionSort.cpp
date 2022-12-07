8cae27316f280e610943fdf0ea195a8550c00465
shizuka1024 <56194005+shizuka1024@users.noreply.github.com>
2022-12-07 17:26:06 +0800

    Update InsertionSort.cpp

--- Sort/InsertionSort.cpp
+++ Sort/InsertionSort.cpp
@@ -17,11 +17,12 @@ void insert(int a[], int n, int i){
 
 int main()
 {
-    int a[] = {28,13,37,5,46};
-    for(int i=1;i<5;i++){
+    int a[] = {28,13,37,5,46,69,9,57,41};
+    int length = sizeof(a)/sizeof(a[0]);
+    for(int i=1;i<length;i++){
         insert(a, a[i], i);
     }
-    for(int i=0;i<5;i++){
+    for(int i=0;i<length;i++){
         printf("%d,", a[i]);
     }
 
