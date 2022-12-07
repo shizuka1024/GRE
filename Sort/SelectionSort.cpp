a04bf7ac3217d771560b748fce6e8e835c95e589
shizuka1024 <56194005+shizuka1024@users.noreply.github.com>
2022-12-07 17:38:19 +0800

    Update SelectionSort.cpp

--- Sort/SelectionSort.cpp
+++ Sort/SelectionSort.cpp
@@ -10,19 +10,21 @@ void swap(int a[],int c,int d){
 int find_min(int a[],int i,int n){
     int k=i;
     for(int j=i+1;j<n;j++){
-        if(a[j]<a[i]) k=j;
+        if(a[j]<a[k]) k=j;
     }
     return k;
 }
 
 int main()
 {
-    int a[] = {28,13,37,5,46};
+    int a[] = {28,13,37,5,46,97,77,99,55,79};
     int length = sizeof(a)/sizeof(a[0]);
+    
     for(int i=0 ;i<length; i++){
         int min_addr = find_min(a, i, length);
         swap(a,i,min_addr);
     }
+    
     for(int i=0;i<length;i++){
         printf("%d,", a[i]);
     }
