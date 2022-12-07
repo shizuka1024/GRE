df907d571b55c6ac882086c56ddfc9038445e399
shizuka1024 <56194005+shizuka1024@users.noreply.github.com>
2022-12-07 15:36:10 +0800

    Create selectionsort.cpp

---
+++ selectionsort.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void swap(int a[],int c,int d){
+	int temp;
+	temp=a[c];
+	a[c]=a[d];
+	a[d]=temp;
+}
+
+int find_min(int a[],int i,int n){
+    int k=i;
+    for(int j=i+1;j<n;j++){
+        if(a[j]<a[i]) k=j;
+    }
+    return k;
+}
+
+int main()
+{
+    int a[] = {28,13,37,5,46};
+    int length = sizeof(a)/sizeof(a[0]);
+    for(int i=0 ;i<length; i++){
+        int min_addr = find_min(a, i, length);
+        swap(a,i,min_addr);
+    }
+    for(int i=0;i<length;i++){
+        printf("%d,", a[i]);
+    }
+    return 0;
+}
