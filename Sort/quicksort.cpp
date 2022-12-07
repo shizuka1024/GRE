ecd27da60bf49ce48cf65eb8a807d3c3e95f1bcc
shizuka1024 <56194005+shizuka1024@users.noreply.github.com>
2022-12-07 16:41:20 +0800

    Create quicksort.cpp

---
+++ Sort/quicksort.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void swap(int a[],int c,int d){
+	int temp;
+	temp=a[c];
+	a[c]=a[d];
+	a[d]=temp;
+}
+
+int partition(int a[],int left,int right){
+    int pivot = a[left];
+    int ptr = left; int i;
+    for(i=left+1;i<=right;i++){
+        if(pivot>=a[i]){
+            ptr++;
+            swap(a,ptr,i);
+        }
+    }
+    a[left] = a[ptr];
+    a[ptr] = pivot;
+    return ptr;
+}
+
+void quicksort(int a[],int left,int right){
+    int mid ;
+    if(left<right){
+        mid = partition(a,left,right);
+        quicksort(a,left,mid-1);
+        quicksort(a,mid+1,right);
+    }
+}
+
+int main()
+{
+    int a[] = {28,13,37,5,46};
+    int length = sizeof(a)/sizeof(a[0]);
+    
+    quicksort(a,0,length-1);
+    
+    for(int i=0;i<length;i++){
+        printf("%d,", a[i]);
+    }
+    return 0;
+}
