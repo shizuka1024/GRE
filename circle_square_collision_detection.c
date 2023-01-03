872fd560164161e685d3b284f025ff14dd006d3f
shizuka1024 <56194005+shizuka1024@users.noreply.github.com>
2023-01-03 19:27:28 +0800

    Create circle_square_collision_detection.c

---
+++ circle_square_collision_detection.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+//give circle's center point(cx, cy), redius r, square's upper-left point (sx, sy), and edge lenthg e
+
+bool collidetec(float cx, float cy, float r, float sx, float sy, float e){
+    float testX = cx;
+    float testY = cy;
+    
+    if(cx < sx) testX = sx;
+    else if(cx > sx + e) testX = sx + e;
+    if(cy > sy) testY = sy;
+    else if(cy < sy - e) testY = sy - e;
+    
+    float distX = cx - testX;
+    float distY = cy - testY;
+    float distance = pow((pow(distX,2)+pow(distY,2)),0.5);
+    
+    if(distance <= r){
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+
+int main()
+{
+    if(collidetec(7,3,1.5,1,5,4)){
+        printf("is collision");
+    }else{
+        printf("no collision");
+    }
+    
+
+    return 0;
+}
